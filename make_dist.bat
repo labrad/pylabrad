00aba291c54c4b0f39ac70435034cc3cf6e7980d
Matthew Neeley <mneeley@gmail.com>
2011-05-04 01:48:52 +0000

    added user access control checking to windows installers

--- make_dist.bat
+++ make_dist.bat
@@ -1,5 +1,5 @@
-setup.py bdist_wininst --target=2.5
-setup.py bdist_wininst --target=2.6
-setup.py bdist_wininst --target=2.7
+setup.py bdist_wininst --target=2.5 --user-access-control=auto
+setup.py bdist_wininst --target=2.6 --user-access-control=auto
+setup.py bdist_wininst --target=2.7 --user-access-control=auto
 setup.py sdist --format=zip
 setup.py sdist --format=gztar
