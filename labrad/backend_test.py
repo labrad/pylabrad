ad9b727f03ed954605abfe92a4c7f0cb6d73f72e
Matthew Neeley <mneeley@gmail.com>
2011-10-12 13:28:59 +0000

    removed obsolete backend_test.py file

--- labrad/backend_test.py
+++
@@ -1,8 +0,0 @@
-import labrad
-
-#cxn = labrad.connect(backend='twisted')
-#print cxn.python_test_server.settings
-
-#cxn = labrad.connect('localhost', backend='asyncore', password='wrong')
-
-cxn = labrad.connect(backend='asyncore', password='wrong')
