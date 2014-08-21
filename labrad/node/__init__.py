c37d529d1ebf2785a6b80c968a317ddd020eab10
Peter O'Malley <ooomalley@gmail.com>
2014-08-21 15:21:18 -0700

    Updated documentation on the node to include the fact that for python servers the server name must match the name given in the info section.

--- labrad/node/__init__.py
+++ labrad/node/__init__.py
@@ -36,6 +36,19 @@ Key -- Value
   javapath -- [full path to the java executable on this system,
                including 'java.exe' at the end]
   packages -- currently unused.
+  
+Changes required on the server:
+See "launchable-server.ini" for a description of how to have a server appear in
+the node. See labrad-servers/Qubit Server/ for an example of a java server.
+For python servers, the same information can be included in the docstring of the
+python server itself. See servers/gpibMockDeviceServer.py in this repository
+for an example.
+***N.B.*** The name given in the server's [info] section MUST MATCH the name 
+given as a class variable in the server class itself. That is, note that the
+name string "GPIB Mock Device Server" appears _twice_. If this is not the case,
+then while the node will still start the server, it will not register that the
+server has been started and you will have to drop the server connection manually
+to restart it.
 
 """
 
