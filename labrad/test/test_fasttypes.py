5cd3e5ab7449c588e7126a49d1ef29148c3c1194
Evan Jeffrey <ejeffrey@physics.ucsb.edu>
2015-04-23 12:57:05 -0700

    Update tests now that they are not crashing

--- labrad/test/test_fasttypes.py
+++ labrad/test/test_fasttypes.py
@@ -8,14 +8,20 @@ def test_int():
     assert ft.flatten(4, 'i') == ('\x00\x00\x00\x04', 'i')
 def test_str():
     assert ft.flatten('abcd', 's') == ('\x00\x00\x00\x04abcd', 's')
-def test_wrong_type():
+def test_wrong_type(): # Figure out the right exceptions to use here
     with pytest.raises(Exception):
         ft.flatten('abcd', 'i')
     with pytest.raises(Exception):
         ft.flatten(4, 's')
 def test_list():
     assert all(T.unflatten(*ft.flatten([1,2,3])) == [1,2,3])
-    assert all(ft.unflatten(T.flatten([1,2,3])) == [1,2,3])
+    (data, tt) = T.flatten([1,2,3])
+    data, tt = str(data), str(tt)
+    assert all(ft.unflatten((data, tt)) == [1,2,3])
+    assert len(ft.flatten(['a', 'bc', 'def'])[0])==22
+def test_list_w_tag():
+    assert str(ft.flatten(['a', 'bc', 'def'])[1])=='*s'
     assert ft.flatten([1,2,3]) == ft.flatten([1,2,3], '*i')
+
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
