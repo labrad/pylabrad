e223c1c169ade230553224752b49a202467192e1
Matthew Neeley <maffoo@google.com>
2015-04-23 07:56:09 -0700

    Modify travis build to use installed pylabrad, not source.
    Review: ejeffrey

--- scripts/test/test.sh
+++ scripts/test/test.sh
@@ -4,13 +4,20 @@ export LABRADHOST=localhost
 export LABRADPASSWORD=testpass
 export LABRADPORT=7777
 
+TEST_SCRIPT=$(dirname $0)/test.py
+ROOT_DIR=$(dirname $0)/../..
+SOURCE_DIR=$ROOT_DIR/labrad
+
 # start labrad manager
 labrad --password ${LABRADPASSWORD} --port ${LABRADPORT} 1>.labrad.log 2>.labrad.err.log &
 sleep 20
 
 # start python test server
-python labrad/servers/test_server.py 1>.test_server.log 2>.test_server.err.log &
+python $ROOT_DIR/labrad/servers/test_server.py 1>.test_server.log 2>.test_server.err.log &
 sleep 20
 
-# run the tests
-python $(dirname $0)/test.py -v . && coverage run --source=labrad $(dirname $0)/test.py -v .
+# run tests to get code coverage
+coverage run --source=$SOURCE_DIR $TEST_SCRIPT -v --pyargs labrad
+
+# run tests again for real, since coverage swallows error codes
+python $TEST_SCRIPT -v --pyargs labrad
