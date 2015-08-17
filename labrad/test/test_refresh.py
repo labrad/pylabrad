6975055e4a9c2a194612c9ba4ba3ad09dd437657
Chris Macklin <chris.macklin@berkeley.edu>
2015-08-17 15:33:32 -0700

    Revert second test server, add third test server with different settings and IDs.

--- labrad/test/test_refresh.py
+++ labrad/test/test_refresh.py
@@ -23,13 +23,25 @@ class RefreshServer2(LabradServer):
     """Second version of the refresh server, with different settings."""
     name = 'Refresh Test Server'
 
-    @setting(10, name='s', returns='s')
+    @setting(1, name='s', returns='s')
     def greet(self, c, name):
         return 'hello, {}!'.format(name)
 
-    @setting(12, a='i', b='i', returns='i')
+    @setting(2, a='i', b='i', returns='i')
     def add(self, c, a, b):
         return a + b
+
+class RefreshServer3(LabradServer):
+    """Third version of the refresh server, with original settings but differing IDs."""
+    name = 'Refresh Test Server'
+
+    @setting(10, returns='s')
+    def greet(self, c):
+        return 'hello!'
+
+    @setting(20, returns='_')
+    def go_away(self, c):
+        pass
 
 def test_refresh():
     with labrad.connect() as cxn:
@@ -81,6 +93,28 @@ def test_refresh():
 
                 assert srv.greet('tester') == 'hello, tester!'
                 assert srv.add(100, 101) == 201
+
+        # pause before refreshing after server disconnect
+        time.sleep(1)
+
+        cxn.refresh()
+        assert not hasattr(cxn, 'refresh_test_server')
+
+        with util.syncRunServer(RefreshServer3()):
+            cxn.refresh()
+            assert hasattr(cxn, 'refresh_test_server')
+
+            # check the old rts we got earlier, as well as the cxn attribute
+            for srv in [rts, cxn.refresh_test_server]:
+                assert hasattr(rts, 'greet')
+                assert same_types(rts.greet.accepts, ['_'])
+                assert same_types(rts.greet.returns, ['s'])
+
+                assert hasattr(rts, 'go_away')
+                assert same_types(rts.go_away.accepts, ['_'])
+                assert same_types(rts.go_away.returns, ['_'])
+
+                assert rts.greet() == 'hello!'
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
