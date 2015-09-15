e969c1abf28b240f45adddfc7a8788eff8698673
Matthew Neeley <maffoo@google.com>
2015-09-14 17:17:33 -0700

    Add a test to verify that server's expireContext method is being called.
    Review: @strj

---
+++ labrad/test/test_server.py
@@ -0,0 +1,33 @@
+import Queue
+import random
+
+import pytest
+
+import labrad
+from labrad.server import LabradServer, setting
+from labrad import util
+
+def test_server_expire_context_method_is_called():
+    """Ensure that server's expireContext method is called when client disconnects."""
+    queue = Queue.Queue()
+
+    class TestServer(LabradServer):
+        name = "TestServer"
+
+        def expireContext(self, c):
+            queue.put(c.ID)
+
+        @setting(100)
+        def echo(self, c, data):
+            return data
+
+    with util.syncRunServer(TestServer()):
+        with labrad.connect() as cxn:
+            # create a random context owned by this connection
+            request_context = (cxn.ID, random.randint(0, 0xFFFFFFFFL))
+            cxn.testserver.echo('hello, world!', context=request_context)
+        expired_context = queue.get(block=True, timeout=1)
+        assert expired_context == request_context
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
