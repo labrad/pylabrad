ab57c8ef86f7f9cf5b321647abba005c62bdda39
Matthew Neeley <mneeley@gmail.com>
2016-07-22 15:49:55 -0700

    Remove MutableFuture. (#273)
    This class was used for backwards-compatibility with older APIs
    before the transition to the standard Future interface.
    
    Review: @ejeffrey

--- labrad/test/test_concurrent.py
+++ labrad/test/test_concurrent.py
@@ -3,21 +3,21 @@ from __future__ import absolute_import
 from concurrent.futures import Future
 import pytest
 
-from labrad.concurrent import MutableFuture
+from labrad.concurrent import map_future
 
 
-def test_mutable_future():
+def test_map_future():
     f = Future()
-    mf = MutableFuture(f)
+    mf = map_future(f, lambda x: x + 1)
     f.set_result(1)
-    assert mf.result() == 1
+    assert mf.result() == 2
 
 
-def test_mutable_future_on_already_completed_future():
+def test_map_future_on_already_completed_future():
     f = Future()
     f.set_result(1)
-    mf = MutableFuture(f)
-    assert mf.result() == 1
+    mf = map_future(f, lambda x: x + 1)
+    assert mf.result() == 2
 
 
 if __name__ == '__main__':
