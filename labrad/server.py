b7f02fae3069539637c00512664fca236021ee11
Matthew Neeley <maffoo@google.com>
2016-09-23 16:24:19 -0700

    Add support for configuring the ThreadedServer thread pool

--- labrad/server.py
+++ labrad/server.py
@@ -583,9 +583,29 @@ class LabradServer(object):
 class ThreadedServer(LabradServer):
     """A LabRAD server that dispatches requests to a thread pool."""
 
+    def __init__(self, pool=None):
+        """Create a new threaded server.
+
+        Requests and lifecycle methods like initServer will be executed on a
+        thread pool instead of in the twisted reactor thread. In addition,
+        accessing self.client from a thread other than the reactor thread will
+        return a synchronous labrad.client.Client object.
+
+        Args:
+            pool (None | concurrent.futures.ThreadPoolExecutor):
+                Thread pool instance to use for server lifecycle methods and
+                request handling. If None, use the default twisted threadpool,
+                which maxes out at 10 threads.
+        """
+        super(ThreadedServer, self).__init__()
+        self.__pool = pool
+
     @inlineCallbacks
     def _dispatch(self, func, *args, **kw):
-        result = yield threads.deferToThread(func, *args, **kw)
+        if self.__pool is None:
+            result = yield threads.deferToThread(func, *args, **kw)
+        else:
+            result = self.__pool.submit(func, *args, **kw)
         if isinstance(result, futures.Future):
             result = yield labrad.concurrent.future_to_deferred(result)
         returnValue(result)
