39742f2dcf36897b040515fcc62350536233d444
Matthew Neeley <maffoo@google.com>
2016-12-23 11:57:22 -0800

    Add support for python 3.6 and test it on travis-ci

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ Operating System :: OS Independent
 Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3.4
 Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
 Topic :: Scientific/Engineering"""
 
 import os
