bc20ea61f5d028a3fc743754b42daa6f55102fec
Matthew Neeley <mneeley@gmail.com>
2017-07-11 22:26:51 -0700

    Expand docstring on labrad.connect. (#335)
    Fixes #334
    Review: @jayich

--- labrad/__init__.py
+++ labrad/__init__.py
@@ -20,7 +20,25 @@ labrad module for python.
 from labrad import backend, client, constants
 
 def connect(host=constants.MANAGER_HOST, port=None, name=None, **kw):
-    """Create a client connection to the labrad manager."""
+    """Create a client connection to the labrad manager.
+
+    Args:
+        host (str): The hostname of the manager. By default, we will use the
+            hostname from the LABRADHOST environment variable, or localhost if
+            the environment variable is not set.
+        port (int): The tcp port of the manager. If None, use the appropriate
+            default value based on the TLS mode.
+        name (str | None): The name of the connection. This name will appear in
+            the manager's connection list. If None, will use a default name
+            based on the current hostname.
+        **kw: Additional keyword args that will be passed to the underlying
+            network code, to specify the TLS and authentication options to use.
+            See `labrad.protocol.connect` for details.
+
+    Returns:
+        (client.Client): labrad client object after the connection has been
+        established.
+    """
     cxn = backend.connect(host=host, port=port, name=name, **kw)
     return client.Client(cxn)
 
