99a3abe9406fbee0383c24e55ef88baf965a4376
kylinzzx <zzxht3@gmail.com>
2019-03-13 14:53:21 +0800

    Fix list_device in python3. (#361)
    Review: @maffoo

--- labrad/devices.py
+++ labrad/devices.py
@@ -303,7 +303,7 @@ class DeviceServer(LabradServer):
     def list_devices(self, c):
         """List available devices."""
         IDs, names = self.deviceLists()
-        return zip(IDs, names)
+        return list(zip(IDs, names))
 
     @setting(2, 'Select Device',
                 key=[': Select first device',
